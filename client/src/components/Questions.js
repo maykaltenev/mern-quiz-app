612ff61a26c060210e4c4a52bd27e9a4e59899ad
Maykal Tenev <michaeltenev@gmail.com>
2022-11-07 21:12:51 +0100

    create a question component

---
+++ client/src/components/Questions.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+
+export default function Questions() {
+    const [checked, setChecked] = useState(undefined)
+    function onSelect() {
+
+    }
+    return (
+        <div className='questions'>
+            <h2 className='text-light'>Simple Question 1</h2>
+            <ul>
+                <li>
+                    <input type="radio"
+                        value={false}
+                        name="options"
+                        id="q1-option"
+                        onChange={onSelect()}
+                    />
+                    <label className='text-primary' htmlFor='q1-option'>option</label>
+                    <div className='check '></div>
+                </li>
+            </ul>
+        </div>
+    )
+}
--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Questions from './Questions'
 export default function Quiz() {
 
     /* On next Handler*/
@@ -14,7 +14,7 @@ export default function Quiz() {
     return (
         <div className='container'>
             <h1 className='title text-light'>Quiz Application</h1>
-
+            <Questions />
             <div className='grid'>
                 <button className='btn prev' onClick={onPrev}>Previous</button>
                 <button className='btn next' onClick={onNext}>Next</button>
