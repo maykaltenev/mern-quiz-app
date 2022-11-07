a817acf42355e7e40e75017f03903f1bcb465835
Maykal Tenev <michaeltenev@gmail.com>
2022-11-07 21:04:27 +0100

    adding buttons and functions to Quiz.js

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,7 +1,24 @@
 import React from 'react'
 
 export default function Quiz() {
+
+    /* On next Handler*/
+    function onNext() {
+        console.log("On next")
+    }
+
+    /* On Prev Handler*/
+    function onPrev() {
+        console.log("On prev")
+    }
     return (
-        <div>Quiz</div>
+        <div className='container'>
+            <h1 className='title text-light'>Quiz Application</h1>
+
+            <div className='grid'>
+                <button className='btn prev' onClick={onPrev}>Previous</button>
+                <button className='btn next' onClick={onNext}>Next</button>
+            </div>
+        </div>
     )
 }
