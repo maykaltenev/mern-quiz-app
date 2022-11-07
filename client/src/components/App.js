d4bae7e10200d215ded93bd27f486c85fe5a0dcb
Maykal Tenev <michaeltenev@gmail.com>
2022-11-07 20:47:45 +0100

    creating RouterProvider and Main, Quiz and Result components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,30 @@
 
-import './styles/App.css';
-
+import '../styles/App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+/* import components */
+import Main from './Main';
+import Quiz from './Quiz';
+import Result from './Result';
+/* react routes */
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div><Main></Main></div>
+  },
+  {
+    path: '/quiz',
+    element: <div><Quiz></Quiz></div>
+  },
+  {
+    path: '/result',
+    element: <div><Result></Result></div>
+  }
+])
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
