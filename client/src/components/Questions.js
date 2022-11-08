a4a1255deedbb3f7a72dc39cec2d7942680bdd95
Maykal Tenev <michaeltenev@gmail.com>
2022-11-08 23:52:55 +0100

    get the result onChecked

--- client/src/components/Questions.js
+++ client/src/components/Questions.js
@@ -2,17 +2,16 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 /** Custom Hook */
 import { useFetchQuestion } from '../hooks/FetchQuestions'
-export default function Questions() {
+export default function Questions({ onChecked }) {
 
-    const [checked, setChecked] = useState(undefined);
     const [{ isLoading, apiData, serverError }] = useFetchQuestion();
 
     const questions = useSelector(state => state.questions.queue[state.questions.trace]);
     useEffect(() => {
-        console.log(questions)
+        // console.log(questions)
     })
-    function onSelect() {
-        // console.log('radio button change')
+    function onSelect(i) {
+        onChecked(i)
     }
 
     if (isLoading) return <h3 className='text-light'>isLoading</h3>;
@@ -30,7 +29,7 @@ export default function Questions() {
                                 value={false}
                                 name="options"
                                 id={`q${i}-option`}
-                                onChange={onSelect()}
+                                onChange={() => onSelect(i)}
                             />
 
                             <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Questions from './Questions'
 
 /** redux store import */
@@ -8,27 +8,34 @@ import { MoveNextQuestion, MovePrevQuestion } from '../hooks/FetchQuestions';
 import { PushAnswer } from '../hooks/setResult';
 
 export default function Quiz() {
+    const [check, setChecked] = useState(undefined);
+
+    const state = useSelector(state => state);
     const { queue, trace } = useSelector(state => state.questions)
     const dispatch = useDispatch();
     useEffect(() => {
-        console.log(trace)
+        console.log(state)
     })
     /* On next Handler*/
     function onNext() {
         /** update the trace value by one using MoveNextAction */
         if (trace < queue.length) {
             dispatch(MoveNextQuestion())
-            dispatch(PushAnswer(1))
+            dispatch(PushAnswer(check))
         }
     }
     /* On Prev Handler*/
     function onPrev() {
-        if (trace > 0) dispatch(MovePrevQuestion())
+        if (trace > 0) dispatch(MovePrevQuestion());
     }
+    function onChecked(check) {
+        setChecked(check)
+    }
+
     return (
         <div className='container'>
             <h1 className='title text-light'>Quiz Application</h1>
-            <Questions />
+            <Questions onChecked={onChecked} />
             <div className='grid'>
                 <button className='btn prev' onClick={onPrev}>Previous</button>
                 <button className='btn next' onClick={onNext}>Next</button>
